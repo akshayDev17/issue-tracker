add04c80f3ffcdf2dce8e04e98f9ab2f0fd8c16e
Abid Ahmad <abid.programming@gmail.com>
2019-12-27 01:54:23 +0530

    Updated Frontend

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,87 +1,34 @@
-import React, {Component} from 'react';
-import {DndProvider} from 'react-dnd';
-import HTML5Backend from 'react-dnd-html5-backend';
+import React, { Component } from 'react';
+import { Route, Switch } from "react-router-dom";
+import Auth from "../hoc/auth";
 import _ from 'lodash';
-import {Board} from './Board';
 
-let _columnId = 0;
-let _cardId = 0;
-
-const initialCards = Array.from({length: 9}).map(() => ({
-  id: ++_cardId,
-  title: `Card ${_cardId}`,
-}));
-
-const initialColumns = ['TODO', 'Doing', 'Done'].map((title, i) => ({
-  id: _columnId++,
-  title,
-  cardIds: initialCards.slice(i * 3, i * 3 + 3).map(card => card.id),
-}));
+import Final from './views/Board/Final.js';
+import LandingPage from "./views/LandingPage/LandingPage.js";
+import LoginPage from "./views/LoginPage/LoginPage.js";
+import RegisterPage from "./views/RegisterPage/RegisterPage.js";
+import NavBar from "./views/NavBar/NavBar";
+import Footer from "./views/Footer/Footer";
 
 class App extends Component {
-  state = {
-    cards: initialCards,
-    columns: initialColumns,
-  };
-
-  addColumn = _title => {
-    const title = _title.trim();
-    if (!title) return;
-
-    const newColumn = {
-      id: ++_columnId,
-      title,
-      cardIds: [],
-    };
-    this.setState(state => ({
-      columns: [...state.columns, newColumn],
-    }));
-  };
-
-  addCard = (columnId, _title) => {
-    const title = _title.trim();
-    if (!title) return;
-
-    const newCard = {id: ++_cardId, title};
-    this.setState(state => ({
-      cards: [...state.cards, newCard],
-      columns: state.columns.map(
-        column =>
-          column.id === columnId
-            ? {...column, cardIds: [...column.cardIds, newCard.id]}
-            : column
-      ),
-    }));
-  };
-
-  moveCard = (cardId, destColumnId, index) => {
-    this.setState(state => ({
-      columns: state.columns.map(column => ({
-        ...column,
-        cardIds: _.flowRight(
-          // 2) If this is the destination column, insert the cardId.
-          ids =>
-            column.id === destColumnId
-              ? [...ids.slice(0, index), cardId, ...ids.slice(index)]
-              : ids,
-          // 1) Remove the cardId for all columns
-          ids => ids.filter(id => id !== cardId)
-        )(column.cardIds),
-      })),
-    }));
-  };
-
+  
   render() {
     return (
-      <DndProvider backend={HTML5Backend}>
-      <Board
-        cards={this.state.cards}
-        columns={this.state.columns}
-        moveCard={this.moveCard}
-        addCard={this.addCard}
-        addColumn={this.addColumn}
-      />
-      </DndProvider>
+      <div>
+        <NavBar />
+     
+        <div style={{ paddingTop: '75px', minHeight: 'calc(100vh - 80px)' }}>
+        
+          <Switch>
+            <Route exact path="/" component={Auth(LandingPage, null)} />
+            <Route exact path="/login" component={Auth(LoginPage, true)} />
+            <Route exact path="/register" component={Auth(RegisterPage, true)} />
+            <Route exact path="/board" component={Auth(Final, true, true)} />
+          </Switch>
+    
+        </div>
+        <Footer />
+        </div>
     );
   }
 }
