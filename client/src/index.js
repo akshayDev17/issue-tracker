fc8c4f0eb2f0959e860fbf51a84493da0783fe64
Abid Ahmad <abid.programming@gmail.com>
2019-12-26 15:17:31 +0530

    React Board

--- client/src/index.js
+++ client/src/index.js
@@ -1,28 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
-
-import LoginComponent from './login.js';
-import RegComponent from './register.js';
-import * as serviceWorker from './serviceWorker';
+import './index.css';
+import App from './components/App';
 
 
-class Root extends React.Component {
-    render() {
-        return (
-            <Router>
-                <button><Link to="/login">LOGIN</Link></button>
-                <button><Link to="/register">REGISTER</Link></button>
-
-                <Route path="/login" ><LoginComponent /></Route>
-                <Route path="/register"><RegComponent /></Route>
-            </Router>
-        );
-    }
-}
-ReactDOM.render(<Root />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById('root'));
